83d28677bdbf0b0726b23e5b2df0dd1b152afd3e
PeyHunter <Nando.goat@gmail.com>
2023-01-23 16:49:26 +0100

    Added description of weather

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 // DATE AND TIME
-
-
-
 let now = new Date();
 
 let days = [
@@ -62,6 +59,7 @@ function displayWeatherCondition(response) {
   );
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#nightTemp").innerHTML = Math.round(response.data.main.temp_min);
 
